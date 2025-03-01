15626f244a9b34fc6349d464b0815b70fe111277
npothapu <106290108+npothapu@users.noreply.github.com>
2025-03-01 08:42:04 -0500

    Delete working-spellcheck-asa-Ford directory

--- working-spellcheck-asa-Ford/tests/mainTest.spec.js
+++
@@ -1,5 +0,0 @@
-const { test, expect } = require('@playwright/test');
-const spellCheckTest = require('./spellCheckTest'); // Import the test logic
-
-// Pass the test object to the function
-spellCheckTest(test);
